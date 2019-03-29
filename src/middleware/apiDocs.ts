79885d7221db2d8b1ad5ad12e5667b1363fd5ee4
Shubham Jain <s.shubjain@gmail.com>
2019-03-29 20:29:29 +0530

    changed Rest to Graphql arch.
    moved to graphql microservice arch from rest

--- src/middleware/apiDocs.ts
+++ src/middleware/apiDocs.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../config/swagger.json";
 
 export const handleAPIDocs = (router: Router) =>
   router.use("/api-docs",
     swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
+    swaggerUi.setup((settings: any) => {
+      settings.SwaggerRoute = "../config/swagger.yml";
+    })
   );
